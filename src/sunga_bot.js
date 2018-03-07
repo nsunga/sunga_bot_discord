df73629a34fd71498531dd2a06a36564b8130e41
Nicholas Sunga <nick.sunga95@gmail.com>
2018-03-07 11:45:38 -0800

    linter bitching

--- src/sunga_bot.js
+++ src/sunga_bot.js
@@ -1,5 +1,7 @@
+/*eslint no-console: ["warn", { allow: ["warn", "error"] }] */
+
 const Discord = require('discord.js');
-const Urban_Dictionary = require('urban-dictionary/urban-dictionary')
+const Urban_Dictionary = require('urban-dictionary/urban-dictionary');
 //const secrets = require('secrets.js');
 const sunga_bot = new Discord.Client();
 const prefix = 'sunga';
@@ -15,15 +17,15 @@ sunga_bot.on('message', message => {
   if (message.content === 'sunga <urban_random>') {
     Urban_Dictionary.random(function (error, entry) {
       if (error) {
-        console.error(error.message)
+        console.error(error.message);
       } else {
-        console.log(entry.word)
-        console.log(entry.definition)
-        console.log(entry.example)
-        message.channel.send(entry.word)
-        message.channel.send(entry.definition)
+        console.log(entry.word);
+        console.log(entry.definition);
+        console.log(entry.example);
+        message.channel.send(entry.word);
+        message.channel.send(entry.definition);
       }
-    })
+    });
   }
 });
 
